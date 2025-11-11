21bb6dff47a7a8ee5245094419eb899a56fbd9f0
MichaelAndrewlo <29CaseM@kgs.org.uk>
2025-11-11 12:36:38 +0000

    Update converters (non twos - complement).py

--- converters (non twos - complement).py
+++ converters (non twos - complement).py
@@ -1,19 +1,26 @@
-def dec_to_bin(value):
+def dec_to(value, base):
     biny = ""
     x = 1
     bits = 0
+    z = 1
     while x <= value:
-        x = x * 2
+        x = x * base
         bits += 1
-    x = x/2
+    x = x / base
     for bit in range(bits):
         if x > value:
             biny += "0"
-            x = x / 2
+            x = x / base
         else:
-            biny += "1"
-            value -= x
-            x = x / 2
+            y = x
+            while y <= value:
+                z += 1
+                y = x * z
+            z -= 1
+            y = x * z
+            biny += str(z)
+            value -= y
+            x = x / base
     return biny
 
 def bin_to_dec(value):
